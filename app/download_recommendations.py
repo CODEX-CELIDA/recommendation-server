f9f1f40fb1433b4e6eb46d79f5180650810599ee
Gregor Lichtner <gregor.lichtner@med.uni-greifswald.de>
2024-01-22 20:11:51 +0100

    fix: removing recommendation path if it exists

--- app/download_recommendations.py
+++ app/download_recommendations.py
@@ -2,11 +2,15 @@
 This script downloads the latest release from the GitHub repository specified in the configuration file.
 """
 import logging
+import shutil
 
 from config import Settings
 from utils import retrieve_release_from_github
 
 settings = Settings()
+
+# first, clean the recommendation directory if it exists
+shutil.rmtree(settings.recommendation_path)
 
 retrieve_release_from_github(
     repository_url=settings.gh_repository, target_path=settings.recommendation_path
